5094b5418efdbf679a35267ef33cf2ef443f44ea
Emma Quist <emma.quist@student.uva.nl>
2021-01-07 19:09:43 +0100

    articles in one df, wo punctuation, splitted and wo stopwords

--- pre_processing.py
+++ pre_processing.py
@@ -1,21 +1,46 @@
 import json
 import pandas as pd
+import numpy as np
+import string
+import re
+import nltk
+from pandas.io.json import json_normalize
 
 articles = []
-file = open("../articles_en_2020_raw.json")
+file = open("../articles_en_2019_raw.nosync.json")
+i = 0
 for article in file:
     articles.append(json.loads(article))
+    i = i + 1
+    if i >= 500:
+        break
 file.close()
 
-dates = [article["date"] for article in articles]
-bodies = [article["body"] for article in articles]
+df_flat = json_normalize(data = articles[0], record_path = 'body', meta = ['id', 'date'])
+for i in range(1, len(articles)):
+    df = json_normalize(data = articles[i], record_path = 'body', meta = ['id', 'date'])
+    df_flat = df_flat.append(df,ignore_index=False, sort=True)
 
-df = pd.DataFrame({"date":dates,"body":bodies})
+def remove_html_punctuation(text):
+    result = string.punctuation  
+    text = re.sub('<[^<]+?>', '', text)
+    no_punct=[words for words in text if words not in result]
+    words_wo_punct=''.join(no_punct)
+    return words_wo_punct
 
-df.head()
+df_flat['content_wo_punct'] = df_flat['content'].apply(lambda x: remove_html_punctuation(x))
+def tokenize(text):
+    split=re.split("\W+",text) 
+    return split
 
-for i in articles[0]:
-    print(i)
+df_flat['content_wo_punct_split']=df_flat['content_wo_punct'].apply(lambda x: tokenize(x.lower()))
 
-#Emms edit
+stopword = nltk.corpus.stopwords.words('english')
 
+def remove_stopwords(text):
+    text=[word for word in text if word not in stopword]
+    return text
+df_flat['content_wo_punct_split'] = df_flat['content_wo_punct_split'].apply(lambda x: remove_stopwords(x))
+
+print(df_flat.head(200))
+print("   ")
