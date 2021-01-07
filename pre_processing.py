a7a357baa00b849bfb038bc6aab93d4c4f4731b6
bram veenman <bramveenman@gmail.com>
2021-01-07 23:32:56 +0100

    open articles in function

--- pre_processing.py
+++ pre_processing.py
@@ -3,16 +3,27 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
+def open_json(filepath):
+    articles = []
+    file = open(filepath)
+    i=0
+    for article in file:
+        articles.append(json.loads(article))
+        i+=1
+        if i > 10: break
+    file.close()
+    return articles
+
 parent_path = Path.cwd().parent
-articles = []
-file = open(parent_path / "articles_en_2020_raw.json")
-for article in file:
-    articles.append(json.loads(article))
-file.close()
+articles = open_json(parent_path / "articles_en_2020_raw.json")
 
 dates = [article["date"] for article in articles]
 bodies = [article["body"] for article in articles]
 
+def unpack_bodies(bodies):
+    pass
+
 df = pd.DataFrame({"date":dates, "body":bodies})
-df.to_csv(parent_path / "dataframe.csv")
+print(df)
+df.to_csv(parent_path / "dataframe2.csv")
 
