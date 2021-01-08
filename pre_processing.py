f1f8bed191040df8eec27b2338cb6a00112a3fa5
bram veenman <bramveenman@gmail.com>
2021-01-08 01:31:19 +0100

    separate body into parts and apply cleaning functions

--- pre_processing.py
+++ pre_processing.py
@@ -1,8 +1,9 @@
 import json
-import pandas as pd
-import numpy as np
+import pandas as pd, numpy as np
+import string, re, nltk
 from pathlib import Path
 
+# load data
 def open_json(filepath):
     articles = []
     file = open(filepath)
@@ -10,20 +11,53 @@ def open_json(filepath):
     for article in file:
         articles.append(json.loads(article))
         i+=1
-        if i > 10: break
+        if i > 100: break
     file.close()
     return articles
 
 parent_path = Path.cwd().parent
 articles = open_json(parent_path / "articles_en_2020_raw.json")
 
+# get dates and bodies for each article
 dates = [article["date"] for article in articles]
 bodies = [article["body"] for article in articles]
 
+# unpack body into title, intro and text
 def unpack_bodies(bodies):
-    pass
+    intros = [next((sec["content"] for sec in body if sec["type"]=="intro"), None) for body in bodies]
+    headers = [next((sec["content"] for sec in body if sec["type"]=="hl1"), None) for body in bodies]
+    paragraphs = [" ".join([sec["content"] for sec in body if sec["type"] == "p"]) for body in bodies]
+    return intros, headers, paragraphs
 
-df = pd.DataFrame({"date":dates, "body":bodies})
+intros, headers, pars = unpack_bodies(bodies)
+
+# put articles into pandas dataframe
+df = pd.DataFrame({"date":dates, "intro":intros, "header":headers, "text": pars})
+
+# functions for cleaning up text
+# remove html punctuation
+def remove_html_punct(text):
+    result = string.punctuation  
+    text = re.sub('<[^<]+?>', '', text)
+    no_punct=[words for words in text if words not in result]
+    words_wo_punct=''.join(no_punct)
+    return words_wo_punct
+
+# split text into separate words
+def tokenize(text):
+    split=re.split("\W+",text) 
+    return split
+
+# remove meaningless common words
+def remove_stopwords(text):
+    stopword = nltk.corpus.stopwords.words('english')
+    text = [word for word in text if word not in stopword]
+    return text
+
+# apply functions
+df['text'] = df['text'].apply(lambda x: remove_html_punct(x))
+df['text'] = df['text'].apply(lambda x: tokenize(x.lower()))
+df[["intro","header","text"]].apply(remove_stopwords, axis=1)
+
 print(df)
-df.to_csv(parent_path / "dataframe2.csv")
-
+# df.to_csv(parent_path / "dataframe2.csv")
