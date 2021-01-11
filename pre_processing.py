1aa5b9e609bd96b262dcdf94fd829a1e5c9519b0
Emma Quist <emma.quist@student.uva.nl>
2021-01-11 15:14:43 +0100

    pre processing

--- pre_processing.py
+++ pre_processing.py
@@ -5,6 +5,9 @@ import string
 import re
 import nltk
 from pandas.io.json import json_normalize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
 
 articles = []
 file = open("../articles_en_2019_raw.nosync.json")
@@ -12,14 +15,18 @@ i = 0
 for article in file:
     articles.append(json.loads(article))
     i = i + 1
-    if i >= 500:
+    if i >= 5:
         break
 file.close()
+
+print(articles[0])
 
 df_flat = json_normalize(data = articles[0], record_path = 'body', meta = ['id', 'date'])
 for i in range(1, len(articles)):
     df = json_normalize(data = articles[i], record_path = 'body', meta = ['id', 'date'])
     df_flat = df_flat.append(df,ignore_index=False, sort=True)
+
+print(df_flat)
 
 def remove_html_punctuation(text):
     result = string.punctuation  
@@ -28,19 +35,25 @@ def remove_html_punctuation(text):
     words_wo_punct=''.join(no_punct)
     return words_wo_punct
 
-df_flat['content_wo_punct'] = df_flat['content'].apply(lambda x: remove_html_punctuation(x))
 def tokenize(text):
     split=re.split("\W+",text) 
     return split
 
-df_flat['content_wo_punct_split']=df_flat['content_wo_punct'].apply(lambda x: tokenize(x.lower()))
-
 stopword = nltk.corpus.stopwords.words('english')
-
 def remove_stopwords(text):
     text=[word for word in text if word not in stopword]
     return text
-df_flat['content_wo_punct_split'] = df_flat['content_wo_punct_split'].apply(lambda x: remove_stopwords(x))
 
-print(df_flat.head(200))
+ps = PorterStemmer()
+def stemming(text):
+    text=[ps.stem(word) for word in text]
+    return text
+
+#def lemmatization
+df_flat['content_wo_punct'] = df_flat['content'].apply(lambda x: remove_html_punctuation(x))
+df_flat['content_wo_punct_split']=df_flat['content_wo_punct'].apply(lambda x: tokenize(x.lower()))
+df_flat['content_wo_punct_split'] = df_flat['content_wo_punct_split'].apply(lambda x: remove_stopwords(x))
+df_flat['content_wo_punct_split_stemmed'] = df_flat['content_wo_punct_split'].apply(lambda x: stemming(x))
+
+print(df_flat.head(20))
 print("   ")
